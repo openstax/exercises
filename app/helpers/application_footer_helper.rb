719faa401a731cc3e7649c465be928d5de158e7a
JP Slavinsky <jpslav@gmail.com>
2013-06-12 17:33:12 -0700

    updated normal content page and home page

--- app/helpers/application_footer_helper.rb
+++ app/helpers/application_footer_helper.rb
@@ -5,7 +5,9 @@ module ApplicationFooterHelper
 
   def copyright_text
     year_range = "2011-#{Time.now.year}".sub(/\A(\d+)-\1\z/, '\1');
-    "Copyright &copy; #{year_range} #{COPYRIGHT_HOLDER}".html_safe
+    link_to copyright_path do
+      "Copyright &copy; #{year_range} #{COPYRIGHT_HOLDER}".html_safe
+    end
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,30 @@ module ApplicationHelper
     options[:class] = klasses;
     options
   end
+
+  def page_heading(heading_text, options={})
+    options[:take_out_site_name] = true if options[:take_out_site_name].nil?
+    options[:sub_heading_text] ||= ""
+    options[:title_text] ||= heading_text + (options[:sub_heading_text].blank? ? 
+                                             "" : 
+                                             " [#{options[:sub_heading_text]}]")
+    
+    @page_title = options[:title_text]
+    @page_title.sub!(SITE_NAME,"").strip! if @page_title.include?(SITE_NAME) && options[:take_out_site_name]
+    
+    return if heading_text.blank?
+    
+    content_for :page_heading do
+      render(:partial => 'shared/page_heading', 
+             :locals => {:heading_text => heading_text, 
+                         :sub_heading_text => options[:sub_heading_text]})
+    end
+  end
+
+  def partial_width_block(&block)
+    content_tag :div, :class => 'partial-width-block' do
+      (capture(&block)).html_safe
+    end
+  end
+
 end
