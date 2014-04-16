af5fdfa8bdad5ad9b9f06cb2230e1c6d22758dca
Dante Soares <dms3@rice.edu>
2014-04-16 14:08:17 -0500

    Added specs, shamelessly copied from Accounts

--- app/access_policies/user_access_policy.rb
+++ app/access_policies/user_access_policy.rb
@@ -2,10 +2,10 @@ class UserAccessPolicy
   # Contains all the rules for which requestors can do what with which User objects.
 
   def self.action_allowed?(action, requestor, user)
-  if !requestor.is_a?(OpenStax::Api::ApiUser) || requestor.is_human?
+  if requestor.is_a?(User) || requestor.is_human?
       # Only admins can currently access the user search API in Exercises
       return requestor.is_admin?
-    else
+  else
       # Only trusted apps can currently access the user search API in Exercises
       return requestor.trusted
     end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -26,14 +26,62 @@ class Api::V1::UsersController < OpenStax::Api::V1::ApiController
     #{json_schema(Api::V1::UserSearchRepresenter, include: :readable)}            
   EOS
   # Using route helpers doesn't work in test or production, probably has to do with initialization order
-  example "#{api_example(url_base: 'https://accounts.openstax.org/api/users/search', url_end: '?q=username:bob%20name=Jones')}"
+  example "#{api_example(url_base: 'https://exercises.openstax.org/api/users/search', url_end: '?q=username:bob%20name=Jones')}"
   param :q, String, required: true, desc: <<-EOS
     The search query string, built up as a space-separated collection of
-    search conditions on different fields.  See the documentation in OpenStax accounts.
+    search conditions on different fields.  Each condition is formatted as
+    "field_name:comma-separated-values".  The resulting list of users will
+    match all of the conditions (boolean 'and').  Each condition will produce
+    a list of users where those users must match any of the comma-separated-values
+    (boolean 'or').  The fields_names and their characteristics are given below.
+    When a field is listed as using wildcard matching, it means that any fields
+    that start with a comma-separated-value will be matched.
+
+    * `username` &ndash; Matches users' usernames.  Any characters matching 
+                 `#{ERB::Util.html_escape (User::USERNAME_DISCARDED_CHAR_REGEX.inspect)}`
+                 will be discarded. (uses wildcard matching)
+    * `first_name` &ndash; Matches users' first names, case insensitive. (uses wildcard matching)
+    * `last_name` &ndash; Matches users' last names, case insensitive. (uses wildcard matching)
+    * `name` &ndash; Matches users' first, last, or full names, case insenstive. (uses wildcard matching)
+    * `id` &ndash; Matches users' IDs exactly.
+    * `email` &ndash; Matches users' emails exactly.
+
+    You can also add search terms without prefixes, separated by spaces.  These terms 
+    will be searched for in all of the prefix categories.  Any matching users will be 
+    returned.  When combined with prefixed search terms, the final results will contain
+    users matching any of the non-prefixed terms and all of the prefixed terms.
+
+    Examples:
+
+    `username:ric` &ndash; returns 'richb' and 'ricardo' users.
+
+    `username:ric name:"Van Buren"` &ndash; returns the 'Ricardo Van Buren' user.
+
+    `ric` &ndash; returns 'richb', 'ricardo', and 'Jimmy Rich' users.
+  EOS
+  param :page, Integer, desc: <<-EOS
+    Specifies the page of results to retrieve, zero-indexed. (default: 0)
+  EOS
+  param :per_page, Integer, desc: <<-EOS
+    The number of users to retrieve on the chosen page. (default: 20)
+  EOS
+  param :order_by, String, desc: <<-EOS
+    A string that indicates how to sort the results of the query.  The string
+    is a comma-separated list of fields with an optional sort direction.  The
+    sort will be performed in the order the fields are given.  
+    The fields can be one of #{Api::V1::SearchUsers::SORTABLE_FIELDS.collect{|sf| "`"+sf+"`"}.join(', ')}.
+    Sort directions can either be `ASC` for 
+    an ascending sort, or `DESC` for a
+    descending sort.  If not provided an ascending sort is assumed. Sort directions
+    should be separated from the fields by a space. (default: `username ASC`)
+
+    Example:
+
+    `last_name, username DESC` &ndash; sorts by last name ascending, then by username descending 
   EOS
   def search
     OSU::AccessPolicy.require_action_allowed!(:search, current_user, User)
-    outputs = SearchUsers.call(params[:q]).outputs
+    outputs = SearchUsers.call(params[:q], params.slice(:page, :per_page, :order_by)).outputs
     respond_with outputs, represent_with: Api::V1::UserSearchRepresenter
   end
 
