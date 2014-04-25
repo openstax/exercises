124a6b48e3f781506b7ebca27239e51a9947b769
Dante Soares <dms3@rice.edu>
2014-04-25 09:13:46 -0500

    Dev controller once again completely independent of the admin controller

--- app/controllers/dev/base_controller.rb
+++ app/controllers/dev/base_controller.rb
@@ -2,11 +2,17 @@
 # License version 3 or later.  See the COPYRIGHT file for details.
 
 module Dev
-  class BaseController < Admin::BaseController
+  class BaseController < ApplicationController
 
     before_filter Proc.new{
       raise SecurityTransgression if Rails.env.production?
     }
+
+    skip_before_filter :authenticate_user!
+    skip_before_filter :require_registration!
+
+    fine_print_skip_signatures :general_terms_of_use,
+                               :privacy_policy
 
   end
 end
