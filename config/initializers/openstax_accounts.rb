647d6979ce0bb27ac562413e228c94c40fae4cc2
Dante Soares <dms3@rice.edu>
2014-05-07 12:38:25 -0500

    Don't display link to admin console if production and not admin

--- config/initializers/openstax_accounts.rb
+++ config/initializers/openstax_accounts.rb
@@ -5,7 +5,7 @@ OpenStax::Accounts.configure do |config|
   config.openstax_application_secret = SECRET_SETTINGS[:openstax_application_secret]
   config.logout_via = :delete
   config.user_provider = ::User
-  config.enable_stubbing = true
+  config.enable_stubbing = false
 end
 
 OpenStax::Accounts::ApplicationController.class_eval do
