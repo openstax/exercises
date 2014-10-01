b0abac7637d239cd8a78e691bf0ae97d96089352
Dante Soares <dms3@rice.edu>
2014-10-01 11:06:55 -0500

    Removed static page view specs

--- spec/views/static_pages/copyright.html.erb_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "static_pages/copyright", :type => :view do
-  it "renders the copyright page" do
-    render
-  end
-end
--- spec/views/static_pages/home.html.erb_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "static_pages/home", :type => :view do
-  it "renders the home page" do
-    render
-  end
-end
