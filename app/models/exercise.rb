2ce0e5ba2e806454e5b29dc38524e49d3cec128e
Dante Soares <dms3@rice.edu>
2015-02-25 20:17:24 -0600

    Added tags (need tests)

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -5,12 +5,11 @@ class Exercise < ActiveRecord::Base
   publishable
   has_attachments
   has_logic :javascript, :latex
+  has_tags
 
   has_many :questions, dependent: :destroy, autosave: true
 
   has_many :list_exercises, dependent: :destroy
-
-  has_many :exercise_tags, dependent: :destroy
 
   protected
 
