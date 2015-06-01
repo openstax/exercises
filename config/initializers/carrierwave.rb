5bf3bcade6417ca232dbf15debf4281982ed8634
Dante Soares <dms3@rice.edu>
2015-06-01 16:01:13 -0500

    Fixes for the fog production config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,19 +2,23 @@ require 'addressable/uri'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
+    require 'fog/aws'
+
     secrets = Rails.application.secrets['aws']['s3']
 
-    config.storage = :fog
-    config.fog_credentials = {
+    fog_credentials = secrets['access_key_id'].blank? ? \
+                        { use_iam_profile: true } : \
+                        { aws_access_key_id:     secrets['access_key_id'],
+                          aws_secret_access_key: secrets['secret_access_key'] }
+
+    config.fog_credentials = fog_credentials.merge(
       provider:              'AWS',
-      aws_access_key_id:     secrets['access_key_id'],
-      aws_secret_access_key: secrets['secret_access_key'],
-      endpoint:              secrets['endpoint_server'],
-      use_iam_profile:       secrets['access_key_id'].blank?
-    }
+      endpoint:              secrets['endpoint_server']
+    )
     config.fog_directory  = secrets['bucket_name']
     config.fog_attributes = { 'Cache-Control' => 'max-age=31536000' }
     config.asset_host = secrets['asset_host']
+    config.storage = :fog
   else
     config.storage = :file
   end
