ad0fc4d5931e921a8cbf09afa24683c0873eacb2
Nathan Stitt <nathan@stitt.org>
2016-04-14 16:30:32 -0500

    Include true-false as a question style

--- lib/style.rb
+++ lib/style.rb
@@ -9,10 +9,11 @@ module Style
   SORTING           = 'sorting'
   FREE_RESPONSE     = 'free-response'
   DRAWING           = 'drawing'
+  TRUE_FALSE        = 'true-false'
 
   def self.all
     [MULTIPLE_CHOICE, MULTIPLE_SELECT, SHORT_ANSWER, FILL_IN_THE_BLANK,
-     POINT_AND_CLICK, MATCHING, SORTING, FREE_RESPONSE, DRAWING]
+     POINT_AND_CLICK, MATCHING, SORTING, FREE_RESPONSE, DRAWING, TRUE_FALSE]
   end
 
 end
