c595eabc56d935a54988e28ef8c1fb8bc1446682
Dante Soares <dante.m.soares@rice.edu>
2016-04-14 10:38:56 -0500

    Terms are now publishable and taggable

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,4 +1,8 @@
 class Term < ActiveRecord::Base
+  publishable
+
+  has_tags
+
   has_many :distractors, foreign_key: 'parent_term_id',
                          dependent: :destroy, inverse_of: :parent_term
   has_many :distracteds, class_name: 'Distractor', foreign_key: 'distractor_term_id',
