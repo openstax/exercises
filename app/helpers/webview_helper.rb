e03e9b840a76936097c9ab815a02e304adbf64c3
Nathan Stitt <nathan@stitt.org>
2016-04-21 12:35:12 -0500

    Set user's data in script tag

---
+++ app/helpers/webview_helper.rb
@@ -0,0 +1,10 @@
+module WebviewHelper
+
+
+  # Generates data for the FE to read as it boots up
+  def client_bootstrap_data
+    current_user.is_anonymous? ? '{}' :
+      Api::V1::UserBootstrapDataRepresenter.new(current_user).as_json
+  end
+
+end
