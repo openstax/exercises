9b52b085414395ee50c2ee5ac55003e91515737c
Dante Soares <dante.m.soares@rice.edu>
2017-11-09 15:06:32 -0600

    Updated accounts-rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109183133) do
+ActiveRecord::Schema.define(version: 20171109210559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -416,12 +416,12 @@ ActiveRecord::Schema.define(version: 20171109183133) do
     t.string   "last_name"
     t.string   "full_name"
     t.string   "title"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.integer  "faculty_status",        default: 0, null: false
+    t.datetime "created_at",                                          null: false
+    t.datetime "updated_at",                                          null: false
+    t.integer  "faculty_status",        default: 0,                   null: false
     t.string   "salesforce_contact_id"
-    t.string   "uuid"
-    t.integer  "role",                  default: 0, null: false
+    t.uuid     "uuid",                  default: "gen_random_uuid()", null: false
+    t.integer  "role",                  default: 0,                   null: false
   end
 
   add_index "openstax_accounts_accounts", ["access_token"], name: "index_openstax_accounts_accounts_on_access_token", unique: true, using: :btree
