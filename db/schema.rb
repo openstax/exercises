50b2428bcd5402386f6a05b96b8cdb90365ce7e5
Dante Soares <dante.m.soares@rice.edu>
2017-12-21 18:20:44 -0600

    Updated accounts-rails to add is_test field to accounts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220232359) do
+ActiveRecord::Schema.define(version: 20171222002015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -425,6 +425,7 @@ ActiveRecord::Schema.define(version: 20171220232359) do
     t.uuid     "uuid",                  default: "gen_random_uuid()", null: false
     t.integer  "role",                  default: 0,                   null: false
     t.citext   "support_identifier"
+    t.boolean  "is_test"
   end
 
   add_index "openstax_accounts_accounts", ["access_token"], name: "index_openstax_accounts_accounts_on_access_token", unique: true, using: :btree
