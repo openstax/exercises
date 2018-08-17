e84178e62b44e1fef9dda21dfd878455bf4d244f
Nathan Stitt <nathan@stitt.org>
2018-08-17 13:55:28 -0500

    Read assets from manfiest

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# initialize Assets
+require 'exercises/assets'
+Exercises::Assets.read_manifest if Rails.env.production?
