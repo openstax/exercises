338f31247e9cd9d6cbf86846a8d738e200ac1031
Nathan Stitt <nathan@stitt.org>
2018-09-06 16:38:19 -0500

    include mathjax script when preview contains math

--- lib/a15k/html_preview.rb
+++ lib/a15k/html_preview.rb
@@ -1,17 +1,29 @@
 require 'erb'
+require 'nokogiri'
 
 module A15k
 
   class HtmlPreview
-    attr_reader :exercise
+    attr_reader :exercise, :html
 
     def initialize(exercise)
       @exercise = exercise
     end
 
+    def template(file)
+      ERB.new(
+        Pathname.new(__FILE__).dirname.join(file).read
+      ).result(binding)
+    end
+
     def generate
-      template = Pathname.new(__FILE__).dirname.join('html_preview.html.erb')
-      ERB.new(template.read).result(binding)
+      @html = template('html_preview.html.erb')
+      @html << template('mathjax.html.erb') if has_math?
+      @html
+    end
+
+    def has_math?
+      Nokogiri::HTML(html).css('math,[data-math]').any?
     end
 
   end
