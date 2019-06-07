2cf6dc57d479dd357bf2208d27c5d220d09d8c32
Ross Reedstrom <reedstrm@gmail.com>
2019-06-07 14:19:43 -0500

    5.2 defaults and rails cache key change

--- app/representers/api/v1/vocabs/term_with_distractors_and_exercise_ids_representer.rb
+++ app/representers/api/v1/vocabs/term_with_distractors_and_exercise_ids_representer.rb
@@ -53,7 +53,7 @@ module Api::V1::Vocabs
     end
 
     def self.cache_key_for(represented, type)
-      "#{represented.cache_key}/#{type}"
+        "#{represented.cache_key}/#{represented.cache_version}/#{type}"
     end
 
     def self.all_cache_keys_for(represented, options = {})
