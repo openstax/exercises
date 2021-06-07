3ec40be758b736cf443488c9631fae838ad76664
Dante Soares <dante.m.soares@rice.edu>
2021-06-07 11:28:55 -0500

    Allow any account to access the status page in non-prod

--- config/initializers/openstax_utilities.rb
+++ config/initializers/openstax_utilities.rb
@@ -2,7 +2,7 @@ OpenStax::Utilities.configure do |config|
   config.status_authenticate = -> do
     authenticate_user!
 
-    next if performed? || current_user.is_administrator?
+    next if performed? || request.host != 'exercises.openstax.org' || current_user.is_administrator?
 
     raise SecurityTransgression
   end
