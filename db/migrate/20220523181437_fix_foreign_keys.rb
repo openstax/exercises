1aba7b6d35d1459ddbfabd9617a629b0d5057de2
Dante Soares <dante.m.soares@rice.edu>
2022-05-24 14:42:14 -0500

    Remove Answers that no longer have a Question

--- db/migrate/20220523181437_fix_foreign_keys.rb
+++ db/migrate/20220523181437_fix_foreign_keys.rb
@@ -5,6 +5,14 @@ class FixForeignKeys < ActiveRecord::Migration[6.1]
     remove_foreign_key :vocab_term_tags, :tags, on_update: :cascade, on_delete: :cascade
     remove_foreign_key :vocab_term_tags, :vocab_terms, on_update: :cascade, on_delete: :cascade
 
+    reversible do |dir|
+      dir.up do
+        Answer.where.not(
+          Question.where('"questions"."id" = "answers"."question_id"').arel.exists
+        ).delete_all
+      end
+    end
+
     add_foreign_key :administrators, :users
     add_foreign_key :answers, :questions
     add_foreign_key :authors, :publications
