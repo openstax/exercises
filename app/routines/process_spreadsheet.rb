6821a3080f452730760bd39ee97bfb3f9579184d
Dante Soares <dante.m.soares@rice.edu>
2024-12-18 10:16:50 -0300

    Added option to return spreadsheet headers when parsing Convert EOC script now finds its columns using the headers

--- app/routines/process_spreadsheet.rb
+++ app/routines/process_spreadsheet.rb
@@ -3,7 +3,7 @@ class ProcessSpreadsheet
 
   protected
 
-  def exec(filename:, offset:, pad_cells: true, &block)
+  def exec(filename:, offset: 1, pad_cells: true, return_headers: false, &block)
     raise ArgumentError, 'A block must be provided' if block.nil?
 
     if File.extname(filename) == 'csv'
@@ -14,14 +14,17 @@ class ProcessSpreadsheet
       method = :each_row_streaming
     end
 
+    args = []
     pad_to_size = 0 if pad_cells
     klass.new(filename).public_send(method).each_with_index do |row, row_index|
-      if pad_cells
+      if return_headers && row_index == 0
+        args << row.map(&:to_s)
+      elsif pad_cells
         row += [nil] * (pad_to_size - row.length) if pad_to_size > row.length
         pad_to_size = row.length
       end
 
-      block.call(row, row_index) if row_index >= offset
+      block.call(*args.concat([row, row_index])) if row_index >= offset
     end
   end
 end
