463a94dcb739a8b5dc67ddbf52ddda57d5c83c8b
Dante Soares <dante.m.soares@rice.edu>
2024-12-19 14:09:12 -0300

    Fix Puma config, initialization

--- config/initializers/access_policies.rb
+++ config/initializers/access_policies.rb
@@ -1,18 +1,20 @@
 require_relative 'doorkeeper'
 
-# User
-OSU::AccessPolicy.register(User, UserAccessPolicy)
-OSU::AccessPolicy.register(Delegation, DelegationAccessPolicy)
+Rails.application.config.after_initialize do
+  # User
+  OSU::AccessPolicy.register(User, UserAccessPolicy)
+  OSU::AccessPolicy.register(Delegation, DelegationAccessPolicy)
 
-# API
-OSU::AccessPolicy.register(Exercise, ExerciseAccessPolicy)
-OSU::AccessPolicy.register(VocabTerm, VocabTermAccessPolicy)
-OSU::AccessPolicy.register(Attachment, AttachmentAccessPolicy)
-OSU::AccessPolicy.register(List, ListAccessPolicy)
-OSU::AccessPolicy.register(ListPublicationGroup, ListPublicationGroupAccessPolicy)
-OSU::AccessPolicy.register(Publication, PublicationAccessPolicy)
-OSU::AccessPolicy.register(CommunitySolution, CommunitySolutionAccessPolicy)
-OSU::AccessPolicy.register(OpenStax::Content::Book, BookAccessPolicy)
+  # API
+  OSU::AccessPolicy.register(Exercise, ExerciseAccessPolicy)
+  OSU::AccessPolicy.register(VocabTerm, VocabTermAccessPolicy)
+  OSU::AccessPolicy.register(Attachment, AttachmentAccessPolicy)
+  OSU::AccessPolicy.register(List, ListAccessPolicy)
+  OSU::AccessPolicy.register(ListPublicationGroup, ListPublicationGroupAccessPolicy)
+  OSU::AccessPolicy.register(Publication, PublicationAccessPolicy)
+  OSU::AccessPolicy.register(CommunitySolution, CommunitySolutionAccessPolicy)
+  OSU::AccessPolicy.register(OpenStax::Content::Book, BookAccessPolicy)
 
-# Doorkeeper
-OSU::AccessPolicy.register(Doorkeeper::Application, Doorkeeper::ApplicationAccessPolicy)
+  # Doorkeeper
+  OSU::AccessPolicy.register(Doorkeeper::Application, Doorkeeper::ApplicationAccessPolicy)
+end
--- config/initializers/doorkeeper_models.rb
+++ config/initializers/doorkeeper_models.rb
@@ -1,21 +1,23 @@
 require_relative 'doorkeeper'
 
-Doorkeeper::Application.class_exec do
-  belongs_to :owner, polymorphic: true
+Rails.application.config.to_prepare do
+  Doorkeeper::Application.class_exec do
+    belongs_to :owner, polymorphic: true
 
-  def is_human?
-    false
-  end
+    def is_human?
+      false
+    end
 
-  def is_application?
-    true
-  end
+    def is_application?
+      true
+    end
 
-  def is_anonymous?
-    false
-  end
+    def is_anonymous?
+      false
+    end
 
-  def is_administrator?
-    false
+    def is_administrator?
+      false
+    end
   end
 end
