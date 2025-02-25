5685117b5f3aa3f961baf50bbcc582d8cb8e745a
Dante Soares <dante.m.soares@rice.edu>
2025-02-25 17:25:40 -0600

    Added task and routine to import Assignable exercises from spreadsheet

--- lib/row_parser.rb
+++ lib/row_parser.rb
@@ -1,7 +1,17 @@
 # Common methods for reading spreadsheets
 module RowParser
-  def split(text, on: /,|\r?\n/)
-    text.to_s.split(on).map(&:strip)
+  # Parses the text using Markdown
+  # Attachments are associated with the given Exercise object
+  def parse(text, exercise)
+    return nil if text.blank?
+
+    text = text.to_s
+
+    kd = Kramdown::Document.new(text.to_s.strip, math_engine: :openstax, attachable: exercise)
+    # If only one <p> tag, remove it and just return the nodes below
+    kd.root.children = kd.root.children.first.children \
+      if kd.root.children.length == 1 && kd.root.children.first.type == :p
+    kd.to_html
   end
 
   def record_failures
@@ -14,5 +24,9 @@ module RowParser
                         Rails.logger.error("Failed rows: #{failures.keys}")
       failures.each { |key, value| Rails.logger.error "Row #{key}: #{value}" }
     end
+  end
+
+  def split(text, on: /,|\r?\n/)
+    text.to_s.split(on).map(&:strip)
   end
 end
