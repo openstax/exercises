8f1f377de21d66aff98ef509174406b8a06a5797
Dante Soares <dante.m.soares@rice.edu>
2025-05-05 10:10:49 -0500

    New cassette for the assignment import test

--- spec/routines/exercises/import/assessments_spec.rb
+++ spec/routines/exercises/import/assessments_spec.rb
@@ -2,8 +2,26 @@ require 'rails_helper'
 require 'vcr_helper'
 
 RSpec.describe Exercises::Import::Assessments, type: :routine, vcr: VCR_OPTS do
+  let(:archive)      { OpenStax::Content::Archive.new(version: '20250226.165223') }
   let(:book_uuid)    { '62a49025-8cd8-407c-9cfb-c7eba55cf1c6' }
   let(:fixture_path) { 'spec/fixtures/sample_import_assessments.xlsx' }
+
+  before do
+    allow_any_instance_of(OpenStax::Content::Abl).to receive(:books).and_return([
+      OpenStax::Content::Book.new(
+        archive: archive,
+        uuid: 'e8668a14-9a7d-4d74-b58c-3681f8351224',
+        version: '964da1b',
+        slug: 'college-success'
+      ),
+      OpenStax::Content::Book.new(
+        archive: archive,
+        uuid: '62a49025-8cd8-407c-9cfb-c7eba55cf1c6',
+        version: '964da1b',
+        slug: 'college-success-concise'
+      )
+    ])
+  end
 
   it 'imports exercises from the spreadsheet and adds Assignable tags' do
     allow(User).to receive(:find) { FactoryBot.create :user, :agreed_to_terms }
@@ -19,10 +37,8 @@ RSpec.describe Exercises::Import::Assessments, type: :routine, vcr: VCR_OPTS do
       'assessment:preparedness:https://openstax.org/orn/book:page/62a49025-8cd8-407c-9cfb-c7eba55cf1c6:97af6b57-0004-4218-99c1-f2cfedea8f30',
       'context-cnxmod:97af6b57-0004-4218-99c1-f2cfedea8f30',
       'book-slug:college-success-concise',
-      'book-slug:preparing-for-college-success',
       'machine-teks:dacf53a6-2b09-49f1-9926-de4efe1049e0',
       'module-slug:college-success-concise:1-1-why-college',
-      'module-slug:preparing-for-college-success:2-1-why-college',
       'teks:T1.1',
     ]
     expect(exercises[0].questions.first.stems.first.content).to eq 'Some question?'
